9c7ebe46e675a72039b15b4c2bcb7b2510331565
rashelleq <64710421+rashelleq@users.noreply.github.com>
2022-12-04 16:05:54 -0700

    added Category and Categories-Preview components. Updated Shop component to have routes.

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from 'react';
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
+
+export const CategoriesContext = createContext({
+  categoriesMap: [],
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // }, []);
+  //DELETE THE USE EFFECT. EVERYTIME YOU RUN IT, IT WILL TRY TO SET NEW VALUES IN DATABASE,
+  //ONLY A TIME THING. ONLY FRONT END - THIS IS HOW IT HAPPENS. GENERALLY NOT HOW IT WORKS
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
